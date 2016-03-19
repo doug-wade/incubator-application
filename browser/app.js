d429152994a889655b61be9bf02ebefffea52053
Snyder <bobsnyder@Roberts-MacBook-Air.local>
2016-03-19 16:55:42 -0700

    essayJS
    essay.js

--- browser/app.js
+++ browser/app.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
+import Essay from './components/Essay';
 
 ReactDOM.render(
-  <HelloWorld />,
+  <div>
+    <HelloWorld />
+    <Essay prompt="What are you doing?" elementId="q1" />
+    <Essay prompt="Who is your target audience?" elementId="q2" />
+    <Essay prompt="How will you make money or sustain your business?" elementId="q3" />
+    <Essay prompt="What do you expect to get out of the Cornish Incubator program?" elementId="q4" />
+    <Essay prompt="How will you further the Cornish mission of Artist, Citizen, Innovator?" elementId="q5" />
+    <Essay prompt="Why this why now?" elementId="q6" />
+  </div>,
   document.getElementById('application-root')
 );
