0e0688250acbce092ca1d3d0969fdc9c4315de4b
doug.wade <doug.wade@redfin.com>
2016-03-19 13:36:03 -0700

    serve react

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import {ReactDOM} from 'react-dom';
-import {HelloWorld} from './components/HelloWorld';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
 
 ReactDOM.render(
   <HelloWorld />,
